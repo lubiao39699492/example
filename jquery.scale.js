11455b266ceb3bf00c0777e493168ae3963ce399
luwake <lubiao39699492@163.com>
2017-05-09 11:03:22 +0800

    Update jquery.scale.js

--- jquery.scale.js
+++ jquery.scale.js
@@ -1,20 +1,29 @@
 (function(){
 
-	var scale = function(el, options){
-		if(options.width && options.height){
-			var width = el.width();
-			var height = (options.height/options.width)*width;
-			el.height(height);
-		}
-	}
-	
-	$.fn.extend({
-		scale:function(options){
+        var resize = function(el, options){
+        	if(!options.width){
+        		options.width = el.data('width');
+        	}
+        	if(!options.height){
+        		options.height = el.data('height');
+        	}
+    		if(options.width && options.height){
+    			var width = el.width();
+    			var height = (options.height/options.width)*width;
+    			el.height(height);
+    		}
+    	};
+        
+    	var scale = function(options){
+    		options = $.extend({width:0,height:0}, options);
 			var $this = $(this);
-			scale($this, options);
+			resize($this, options);
 			$(window).resize(function(){
-				scale($this, options);
+				resize($this, options);
 			});
-		}
-	});
-})();
+    	}
+    	
+    	$.fn.extend({
+    		scale:scale
+    	});
+    })();
