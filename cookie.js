c62bc3fe4caa8a4d2024732a50bd3351189d590f
luwake <lubiao39699492@163.com>
2017-12-23 15:11:59 +0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,21 +1,21 @@
-function cookieSave(n, v, mins, dn, path) {
-    if (n) {
-        if (!mins) mins = 7 * 24 * 60;
+function cookieSave(name, value, second, domain, path) {
+    if (name) {
+        if (!second) second = 7 * 24 * 60;
         if (!path) path = "/";
         var date = new Date();
-        date.setTime(date.getTime() + (mins * 60 * 1000));
+        date.setTime(date.getTime() + (second * 1000));
         var expires = "; expires=" + date.toGMTString();
-        if (dn) dn = "domain=" + dn + "; ";
-        document.cookie = n + "=" + v + expires + "; " + dn + "path=" + path;
+        if (domain) domain = "domain=" + domain + "; ";
+        document.cookie = name + "=" + value + expires + "; " + domain + "path=" + path;
     }
 }
-function cookieGet(n) {
-    var name = n + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+function cookieGet(name) {
+    var name = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') cookie = cookie.substring(1, cookie.length);
+        if (cookie.indexOf(name) == 0) return cookie.substring(name.length, cookie.length);
     }
     return "";
 }
